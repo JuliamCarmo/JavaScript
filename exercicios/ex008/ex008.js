2b760d751ab5b3aa1861f3097f690ddbca329946
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-01-13 12:50:02 -0300

    exercícios

---
+++ exercicios/ex008/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62
+console.log(`A velocidade do seu carro é de ${vel} km/h`)
+  if(vel > 60){ //condição simples
+      console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+  }
+console.log('Dirija com cuidado!')
---
+++ exercicios/ex009/ex009.js
@@ -0,0 +1,7 @@
+var país = 'Brasil'
+console.log(`Eu moro no ${país}`)
+if(país == 'Brasil'){
+  console.log('Você é Brasileiro!')
+}else{
+  console.log('Você é Estrangeiro!')
+}
