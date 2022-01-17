46b8d417b9fec6994f5539030dfc8af93b69bd78
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-01-17 16:09:58 -0300

    exercícios

---
+++ exercicios/ex013/ambiente.js
@@ -0,0 +1,13 @@
+var c = 1
+
+do {
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+} while (c <= 10)
+
+/* Outra opção, com a mesma função
+ while(c <= 10){
+  console.log(`Passo ${c}`)
+  c++ // c = c + 1
+} 
+*/
---
+++ exercicios/ex014/ambiente.js
@@ -0,0 +1,5 @@
+console.log('Vai começar...')
+for (c = 1; c <= 4; c++) {
+  console.log(c)
+}
+console.log('Fim!')
