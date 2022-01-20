8bcbbabc0d80b45a54ab6520e83470ce5efc4024
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-01-20 18:40:21 -0300

    exercício

--- exercicios/ex008/ex008.js
+++ exercicios/ex008/ex008.js
@@ -1,6 +1,8 @@
 var vel = 62
+
 console.log(`A velocidade do seu carro é de ${vel} km/h`)
-  if(vel > 60){ //condição simples
-      console.log('Você ultrapassou a velocidade permitida. MULTADO!')
-  }
+
+if (vel > 60) { //condição simples
+  console.log('Você ultrapassou a velocidade permitida. MULTADO!')
+}
 console.log('Dirija com cuidado!')
--- exercicios/ex013/ambiente.js
+++ exercicios/ex013/ambiente.js
@@ -5,9 +5,10 @@ do {
   c++ // c = c + 1
 } while (c <= 10)
 
-/* Outra opção, com a mesma função
- while(c <= 10){
-  console.log(`Passo ${c}`)
-  c++ // c = c + 1
-} 
-*/
+
+/*while(c <= 10){
+ console.log(`Passo ${c}`)
+ c++ // c = c + 1
+}*/
+
+
