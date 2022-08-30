3112de0886f3127cb929d24a70bed187eebd3478
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-08-30 17:17:03 -0300

    exercício - Origamid

---
+++ exercicios/ex019/script.js
@@ -0,0 +1,10 @@
+console.log('Teste')
+
+const nome = 'JavaScript'  //"const" é uma variável que não varia, é constante
+console.log(nome)
+
+let fatec = 'Diadema' //"let" pode variar, obedecendo ao ultimo comando da variável
+fatec = 'São Caetano'
+console.log(fatec)
+
+
