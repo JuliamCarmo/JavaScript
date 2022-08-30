cb3c031be713ee0b1a20f87ba3fc883db7ba42f9
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-08-30 17:36:36 -0300

    exercício - Origamid

--- exercicios/ex019/script.js
+++ exercicios/ex019/script.js
@@ -1,10 +1,19 @@
-console.log('Teste')
+//Manipulação do DOM. 
 
-const nome = 'JavaScript'  //"const" é uma variável que não varia, é constante
-console.log(nome)
+//Seleciona o elemento  
+const nav = document.querySelector('nav')
+const produtos = document.querySelector('.produtos a')
 
-let fatec = 'Diadema' //"let" pode variar, obedecendo ao ultimo comando da variável
-fatec = 'São Caetano'
-console.log(fatec)
+//E muda o que você quer nele, atravez de propriedades e metodos.
+console.log(nav)
+console.log(produtos)
 
+console.log(produtos.innerHTML)
+console.log(produtos.href)
 
+produtos.remove() //FUNÇÃO para remover elementos
+
+nav.style.backgroundColor = "grey"
+nav.style.padding = "1rem"
+
+nav.classList.add("ativo")
