cefacb72c81ec802c7e486d81da51780de449b70
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-08-30 18:02:07 -0300

    exercício

---
+++ exercicios/ex020/script.js
@@ -0,0 +1,19 @@
+//String
+const text = 'JavaScript'
+const text2 = 'Isso é uma "String!"'
+
+console.log(text +' '+ text2)
+
+//Number
+let total = 0
+const compra = 30
+const imposto = 10
+total = compra + imposto
+
+console.log(total)
+
+const string = "20" + "20"
+const numbers = 20 + 20
+const transformar = Number("20") + 20
+
+console.log(transformar)
