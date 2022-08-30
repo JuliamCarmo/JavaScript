4faa49742c346f15ef16948fa93d1eef39cf6d4d
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-08-30 18:15:48 -0300

    object

---
+++ exercicios/ex021/script.js
@@ -0,0 +1,25 @@
+//Objects
+const nome = 'O Senhor dos Anéis'
+const ano = 1954
+const autor = 'J. R. R. Tolkien'
+
+const nomeFilme = 'O Senhor dos Anéis'
+const anoFilme = 2001
+const diretorFilme = 'Peter Jackson'
+
+
+const livro = {
+  nome: 'O Senhor dos Anéis',
+  ano: 1954,
+  autor: 'J. R. R. Tolkien'
+}
+
+const filme = {
+  filme: 'O Senhor dos Anéis',
+  ano: 2001,
+  autor: 'J. R. R. Tolkien',
+  diretor: 'Peter Jackson'
+}
+
+console.log(nome === nome)
+console.log(filme)
