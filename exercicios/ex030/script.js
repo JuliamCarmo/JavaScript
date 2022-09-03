476e470c03f60039dbafb688ba3a0678593fa9c1
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-09-03 10:29:34 -0300

    querySelectorAll

---
+++ exercicios/ex030/script.js
@@ -0,0 +1,10 @@
+const links = document.querySelectorAll('nav a')
+
+function logHref(item){
+  const href = item.href
+  console.log(href)
+}
+
+links.forEach(logHref)
+
+console.log(links[links.length -1])
