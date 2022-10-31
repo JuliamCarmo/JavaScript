a453c8de8bb2bf8c3ab1265f3ee9794bc59a5d01
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-10-31 20:40:45 -0300

    commit

---
+++ exercicios/ex000/boolean e condicionais/script.js
@@ -0,0 +1,29 @@
+var possuiGraduação = true;
+var possuiDoutorado = true;
+
+if (possuiDoutorado){
+  console.log('Possui graduação e doutorado')
+}else if (possuiGraduação){
+  console.log('Possui graduação mas não possui doutorado')
+}else {
+  console.log('Não possui graduação')
+}
+
+/*Formas de dar false no teste de if
+if(false)
+if(0) ou -0
+if(NaN)
+if(null)
+if(undefined)
+if('') string vazia
+
+Todo oo resto é verdadeiro - true
+*/
+
+/*Inverter os valores !
+if(!true)
+if(!1)
+if(!'')
+if(!undefined)
+if(!!' ') !! Serve para VERIFICAR se é true ou false
+*/
