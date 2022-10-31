493d4a432113916f6096c1d0fa2ee716f0b20275
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2022-10-31 10:16:01 -0300

    Variaveis

---
+++ variaveis/script.js
@@ -0,0 +1,25 @@
+//Declarar variavel com nome
+var nome = 'Julia';
+
+//Declarar variavel com idade e facul
+var idade = 21;
+possuiFaculdade = true;
+
+//Calculo de preço
+var preco = 25.00,
+    itensComprados = 5,
+    total = itensComprados * preco;
+console.log(total);
+
+//Declarar variavel com comida e não atribuir valor
+var comidaFavorita;
+
+//Atribuir valor
+comidaFavorita = 'Pizza';
+
+//Variaveis sem valores
+var casa, numero, cidade, bairro, cpf;
+
+console.log(nome, idade, possuiFaculdade, comidaFavorita);
+
+
---
+++ variaveis/script2.js
@@ -0,0 +1,18 @@
+//Declare uma variavel contendo uma string
+var nome = 'Julia';
+
+//Declare uma variavel contendo um numero dentro de uma string
+var ano = '2022';
+
+//Declare uma variavel com a sua idade
+var idade = 21;
+console.log(`A idade de Julia é de ${idade} anos`);
+
+//Declare nome e sobrenome
+var sobrenome = 'Carmo';
+var nomeCompleto = `${nome} ${sobrenome}`;
+
+//Declare a variavel com a frase: It's time
+var frase = "it's time" //ou `it's time` ou 'it\'\s time'
+
+console.log(nome, ano, nomeCompleto, frase)
