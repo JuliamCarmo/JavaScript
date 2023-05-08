f9280d00af071a682744b31feb049aa1dc134562
Julia Carmo <93735857+JuliamCarmo@users.noreply.github.com>
2023-05-08 09:28:51 -0300

    arrays pilha

---
+++ origamid/ex10/ex10.js
@@ -0,0 +1,9 @@
+var videogame = ['PS', 'xbox', 'nintendo']
+
+console.log(videogame);
+
+//pilha (remove e add ao final)
+console.log(videogame.pop()) //remove do final e retorna
+console.log(videogame.push('switch')); //adiciona ao final
+
+console.log(videogame.length)
